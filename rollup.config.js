476ef5556d9b679b1d872ef291ea6f1de6efbd74
brianchesko <cheskobrian@gmail.com>
2021-06-27 02:31:25 -0400

    changes to package.json to get scripts working, initial page with interworking css, js, html

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ import pkg from './package.json';
 // Configs
 var configs = {
 	name: 'brianCheskoWebpage',
-	files: ['main.js', 'detects.js', 'another-file.js'],
+	files: ['main.js', 'detects.js', 'onload.js'],
 	formats: ['iife', 'es', 'amd', 'cjs'],
 	default: 'iife',
 	pathIn: 'src/js',
