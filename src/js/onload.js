9a2aa2cefe6374c686fdc22645f68dce7132c937
brianchesko <cheskobrian@gmail.com>
2021-06-28 00:58:21 -0400

    re-add img, svg scripts to package.json; add some css variables; add WIP header with fading in background image

--- src/js/onload.js
+++ src/js/onload.js
@@ -4,9 +4,5 @@ const bannerWrapper = document.getElementById('banner_wrapper');
 
 if (bannerWrapper) {
     // throw this on with js to ensure page has fully loaded
-    console.log('aaa')
     bannerWrapper.classList.add('active');
-} else {
-    
-    console.log('bbbbb')
 }
