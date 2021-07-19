ff6b66260df7d7951ed32549f94f63eeeea5a803
brianchesko <cheskobrian@gmail.com>
2021-07-19 00:41:58 -0400

    add differential media files, restructure js files, add modal to image clicks

--- src/js/components/onclick-listeners.js
+++ src/js/components/onclick-listeners.js
@@ -1,5 +1,5 @@
 // add visibility toggle to show coursework
-document.getElementById('coursework_show-btn').addEventListener('click', () => {
+const toggleCoursework = () => {
 	const button = document.getElementById('coursework_show-btn');
 	const list = document.getElementById('coursework_list');
 	if (button.classList.contains('visible')) {
@@ -9,8 +9,18 @@ document.getElementById('coursework_show-btn').addEventListener('click', () => {
 	}
 	button.classList.toggle('visible');
 	list.classList.toggle('visible');
-}, false);
+};
 
+const closeModal = (event) => {
+	if (event.target != event.currentTarget) {
+		return; // only process if we clicked EXACTLY the x or clickthrough, nothing on the modal
+	}
+	const modal = document.getElementById('image-modal');
+	modal.classList.remove('image-modal__visible');
+};
 
+document.getElementById('coursework_show-btn').addEventListener('click', toggleCoursework, false);
+document.getElementById('image-modal').addEventListener('click', closeModal, false);
+document.getElementById('image-modal_close-button').addEventListener('click', closeModal);
 
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,3 @@
 import './components/_matches.polyfill.js';
+import './components/onload.js';
 import './components/onclick-listeners.js';
